a7c514d24ffd3c554dcfe666ece76d9543769bc8
PARK, YUJIN <jinee525@naver.com>
2025-08-29 17:48:34 +0900

    프로젝트 내용 개선 및 AI 봇 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,17 @@ import "./globals.css";
 import { ReactNode } from "react";
 import { Global } from "@emotion/react";
 import { globalStyles } from "./globalStyles";
+import Chatbot from "@/components/Chatbot";
+import Header from "@/components/Header";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Global styles={globalStyles} />
+        <Header />
         {children}
+        <Chatbot />
       </body>
     </html>
   );
